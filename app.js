dd1047aed20a2fdddf0a84f8bcc9e6e72133b8c6
Alejandro Di Stefano <addistefano@live.com.ar>
2025-08-28 21:02:28 -0300

    Clase 05

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+
 import homeRouter from './routes/home.router.js'
 import studentRouter from './routes/student.router.js'
 import authRouter from './routes/auth.router.js';
+import apiV1Router from './routes/api.v1.router.js';
+import advanceRouter from './routes/advancedRouter.js';
+import processRouter from './routes/process.router.js';
+
 import logger from './middleware/logger.middleware.js'
 import { connectAuto } from './config/db/connect.config.js'
 import session from 'express-session';
@@ -10,6 +15,7 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import passport from 'passport';
 import { initPassport } from './config/auth/passport.config.js'
+import env, { validateEnv } from './config/env.config.js';
 
 const app = express();
 
@@ -24,6 +30,10 @@ app.use(cookieParser('clave_secreta'));
 
 const startServer = async () => {
 
+    // Validar la existencia de las variables de entorno importantes.
+    validateEnv();
+
+    // Se conecta a la base de datos
     await connectAuto();
 
     const store = MongoStore.create({
@@ -48,13 +58,36 @@ const startServer = async () => {
     initPassport();
     app.use(passport.initialize());
 
+
     // Llamadas al enrutador
     app.use('/', homeRouter);
     app.use('/auth', authRouter);
     app.use('/student', studentRouter);
 
+
+    // Agrupar Routers versionados
+    app.use('/api/v1', apiV1Router);
+    app.use('/advanced', advanceRouter);
+    app.use('/process', processRouter);
+
+
     app.use((req, res) => {
         res.status(404).json({ error: "Pagina no encontrada.!" });
+    })
+
+
+    // Manejo de señales y errores globales
+    process.on('unhandledRejection', (reason) => {
+        console.error('[process] Unhandled Rejection ', reason);
+    })
+
+    process.on('uncaughtException', (err) => {
+        console.error('[process] Uncaught Exception ', err);
+    })
+
+    process.on('SIGINT', () => {
+        console.log('\n[process] SIGINT recibido. Cerrando...')
+        process.exit(0);
     })
 
     app.listen(PORT, () => console.log(`✅ Servidor escuchando en http://localhost:${PORT}`));
---
+++ middleware/policies.middleware.js
@@ -0,0 +1,7 @@
+// Autorizacion por rol simple
+export const policies = (...roles) => (req, res, next) => {
+    // passport coloca al user en req.user
+    if(!req.user) return res.status(401).json({error: 'No Autorizado'});
+    if(!roles.includes(req.user.role)) return res.status(403).json({error: 'Prohibido el paso'});
+    next();
+};
